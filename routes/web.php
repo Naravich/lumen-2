c1d16befbce7a5b47304e96e4f38d60d4b183b29
seagame <naravich@carro.co>
2018-09-25 17:00:33 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/hello/world', function () use ($router) {
+    return "HELLO SEAGAME WORLD";
+});
+
+$router->get('/hello/{name}', ['middleware' => 'hello', function ($name) use ($router) {
+    return "HELLO MR.{$name}";
+}]);
+
+$router->get('/request', function (Illuminate\Http\Request $request) {
+    return "Hello " . $request->get('name', 'stranger');
+});
+
+// $router->get('/response', function (Illuminate\Http\Request $request) {
+//     return (new Illuminate\Http\Response('Hello stranger', 200))
+//     ->header('Content-Type', 'text/plain');
+// });
+
+$router->get('/response', function (Illuminate\Http\Request $request) {
+    if ($request->wantsJson()) {
+        return response()->json(['greeting' => 'Hello Stranger Res']);
+    }
+    return (new Illuminate\Http\Response('Hello stranger', 200))
+    ->header('Content-Type', 'text/plain');
+});
+    
